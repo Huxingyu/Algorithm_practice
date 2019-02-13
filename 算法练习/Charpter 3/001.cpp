d4920f038dc3bca68c6773e0140cacd52572a484
huxingyu <huxingyu@huxingyu.top>
2019-02-13 14:37:33 +0800

    1

---
+++ 算法练习/Charpter 3/001.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int n,i=0;
+    scanf("%d",&n);
+    while(n!=1){
+        if(n%2==0){
+            n=n/2;
+            i++;
+        }
+        else{
+            n=(3*n+1)/2;
+            i++;
+        }
+    }
+    printf("%d",i);
+    return 0;
+}
+
+//pat b1001;
+//优化，可以用一个while(){n++}代替两个小的n++;
---
+++ 算法练习/Charpter 3/002.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int n,school[100000]={0};
+    int j,sorce;
+    int max=0;
+    scanf("%d",n);
+    for(int i=0;i<n;i++){
+        scanf("%d %d",&j,&sorce);
+        school[j] = school[j] + sorce;
+        if(school[j]>max){
+            max = school[j];
+        }
+    }
+    printf("%d %d",j,max);
+    return 0;
+}
