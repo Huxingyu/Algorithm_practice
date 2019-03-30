3015d02f7129f08927b82f8daa4b221596602f87
huxingyu <huxingyu@huxingyu.top>
2019-03-30 23:34:34 +0800

    1

--- 算法练习/Charpter 6/043.cpp
+++ 算法练习/Charpter 6/043.cpp
@@ -8,5 +8,8 @@ int main(){
     map<string,int> mp;
     mp.insert(make_pair("hu",1));
     mp.insert(pair<string,int>("xingyu",2));
-    
+    for(map<string,int>::iterator it=mp.begin();it!=mp.end();it++){
+        cout<< it->first <<" "<< it->second <<endl;
+    }
+    return 0;
 }
